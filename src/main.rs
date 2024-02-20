d26ed85d5c745d267c41dab47b8f588352af7efc
Quinn <i486767>
2024-02-20 21:22:13 +0100

    Finished RCA processing and retrieval of blocks, QOL improvements needed, split into lib

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use miniz_oxide::inflate;
 use fastnbt::{self, IntArray, LongArray, Value};
 use serde::Deserialize;
 
+const SECTION_SIZE: usize = 4096; // blocks == 16 * 16 * 16
+const DATA_SECTOR_SIZE: usize = 4096; // Different values for clarity; this describes the individual data sectors in the file which are always 4096 bytes
+const BLOCK_BIT_SIZE: usize = 4;
+const CHUNK_STRIP_SIZE: usize = 16;
 use std::{
     collections::{
         hash_map::{Values, ValuesMut},
@@ -50,30 +54,35 @@ pub struct CompressedChunk
 }
 
 
-// /// The represents part of a chunk's nbt data stored in the region file
-// ///
-// /// See <https://minecraft.fandom.com/wiki/Chunk_format#NBT_structure>
-// #[derive(Deserialize, Clone, Debug)]
-// pub struct BlockState {
-//     #[serde(rename = "Name")]
-//     pub name: String,
-//     #[serde(rename = "Properties")]
-//     pub properties: Option<Value>,
-// }
+/// The represents part of a chunk's nbt data stored in the region file
+///
+/// See <https://minecraft.fandom.com/wiki/Chunk_format#NBT_structure>
+#[derive(Deserialize, Clone, Debug)]
+pub struct Block {
+    #[serde(rename = "Name")]
+    pub name: String,
 
-// #[derive(Deserialize, Clone, Debug)]
-// #[serde(rename_all = "PascalCase")]
-// pub struct BlockStates {
-//     pub palette: Vec<BlockState>,
-//     pub data: Option<LongArray>,
-// }
+    #[serde(rename = "Properties")]
+    pub properties: Option<Value>,
+}
 
-// #[derive(Deserialize, Clone, Debug)]
-// #[serde(rename_all = "PascalCase")]
-// pub struct ChunkSection {
-//     pub block_states: Option<BlockStates>,
-//     pub y: i8,
-// }
+#[derive(Deserialize, Clone, Debug)]
+pub struct BlockStates {
+    #[serde(rename = "palette")]
+    pub palette: Vec<Block>,
+
+    #[serde(rename = "data")]
+    pub data: Option<LongArray>,
+}
+
+#[derive(Deserialize, Clone, Debug, )]
+pub struct ChunkSection {
+    #[serde(rename = "block_states")]
+    pub block_states: Option<BlockStates>,
+
+    #[serde(rename = "Y")]
+    pub y: i8,
+}
 
 /// The represents that chunk's nbt data stored in the region file
 ///
@@ -87,7 +96,7 @@ pub struct Chunk {
     pub status: String,
 
     #[serde(rename = "sections")]
-    pub sections: Vec<Value>, // todo use serde json to get the values, preferrably not but fuck it we ball. can always imrpove it as long as the logic stays the same we can fix it later
+    pub sections: Vec<ChunkSection>, // todo use serde json to get the values, preferrably not but fuck it we ball. can always imrpove it as long as the logic stays the same we can fix it later
 
     #[serde(rename = "zPos")]
     pub z_pos: i32,
@@ -112,7 +121,7 @@ impl RegionChunks
         // very very very very bad fix. Improve / use the offical solution, for now i am lazy.
         for chunk in &self.chunks // max 1024
         {
-            if (chunk.x_pos == x && chunk.z_pos == z)
+            if chunk.x_pos == x && chunk.z_pos == z
             {
                 return chunk;
             }
@@ -121,35 +130,6 @@ impl RegionChunks
         return &self.chunks[0]; // very bad and naughty
         // return &self.chunks[((x & 31) + (z & 31) * 32) as usize]
     }
-}
-
-
-fn parse_region_file_header(reader: &mut dyn Read) -> Result<RegionHeader, Error>
-{
-    // byte buffer for the location entries; initialize with 4096 0's. (0u8 i.e. a U8 with the value of zero repeated 4096 times)
-    let mut buffer = [0u8; 4096];
-    let mut header_location_entries = Vec::new();
-
-    // Read 4096 bytes from reader into buffer
-    reader.read(&mut buffer)?;   
-
-    // Loop over buffer in chunks of 4 bytes (see wiki)
-    for byte in buffer.chunks(4)
-    {        
-        header_location_entries.push(RegionHeaderLocationEntry 
-        {
-            offset: big_endian!(&[0, byte[0], byte[1], byte[2]]),
-            sector_count: byte[3],
-        });
-    }
-
-    // Empty read away the timestamps. For future use
-    let mut tmp_buffer = [0u8; 4096];
-    reader.read(&mut tmp_buffer);
-    
-    let region_header = RegionHeader {locations: header_location_entries};
-    
-    Ok(region_header)
 }
 
 
@@ -165,7 +145,8 @@ fn get_encoded_chunks(data: &mut Vec<u8>, locations: Vec<RegionHeaderLocationEnt
         }
 
         // -2 for accounting of the two 4096 bytes header sectors at the start of the file
-        let chunk_start = (location.offset - 2) as usize * 4096_usize; 
+        let chunk_start = (location.offset - 2) as usize * DATA_SECTOR_SIZE; 
+
         // chunk length field = 4 bytes total
         let chunk_length = big_endian!(&data[chunk_start..(chunk_start + 4)]); 
 
@@ -191,18 +172,69 @@ fn get_chunk_nbt(data: &mut Vec<u8>) -> Result<Chunk, Error> {
     let uncompressed = uncompressed.map_err(|_| Error::from(ErrorKind::UnexpectedEof))?;
 
     let chunk: Chunk = fastnbt::from_bytes(&uncompressed).unwrap();
-    // let chunk_sections_nbt: Vec<ChunkSection> = fastnbt::from_bytes(&uncompressed).unwrap();
-
-    // chunk.sections = chunk_sections_nbt;
 
     Ok(chunk)
 }
+
+fn get_blocks_in_section(chunk_section: BlockStates) -> Result<Vec<Block>, Error>
+{
+    let mut block_states = Vec::new();
+    let pallete: Vec<Block> = chunk_section.palette;
+
+    if chunk_section.data.is_none()
+    {  
+        return Ok::<Vec<Block>, Error>(vec![pallete[0].clone(); SECTION_SIZE]);    // section is 16*16*16. fill with first block in pallete according to wiki
+    }
+
+    let data: LongArray = chunk_section.data.unwrap();
+    
+    for i in 0..data.len()
+    {
+        let mut val = data[i];
+
+        for i in 0 .. CHUNK_STRIP_SIZE // length of one strip of blocks
+        {
+            // extract the first 4 active bits, could grow if pallete size grows beyond decimal 15, so if pallete size has 16 blocks, we need to increase the bit size for an 
+            // index to 5 bits, checking this with a formula should be doable.
+            let block_pallete_index = (val & 0b1111) as u8;
+            let extracted_block = &pallete[block_pallete_index as usize];
+
+            block_states.push(extracted_block.clone());
+
+            // shift 4 bits minimum to next block, may change when pallete size also increases to exceed a 4 bit limit...
+            val >>= BLOCK_BIT_SIZE;
+        }
+    }
+
+    Ok(block_states)
+}
+
+fn get_blocks_in_chunk(region_chunks: &RegionChunks, chunk_x: i32, chunk_z: i32) -> Vec<Block>
+{
+
+    let chunk: &Chunk = region_chunks.get_chunk(chunk_x, chunk_z);
+    let mut block_states_chunk = Vec::new();
+
+    for i in 0 .. chunk.sections.len()
+    {
+        if chunk.sections[i].block_states.is_some()
+        {
+            let chunk_section = chunk.sections[i].block_states.clone().unwrap();
+            let mut section_blocks: Vec<Block> = get_blocks_in_section(chunk_section).expect("Failure retreiving blocks from section");
+    
+            block_states_chunk.append(&mut section_blocks)
+        }
+    }
+
+    return block_states_chunk
+}
+
 
 fn decode_chunks(compressed_chunks: &mut Vec<CompressedChunk>) -> Result<Vec<Chunk>, Error>
 {
     let mut decoded_chunks = Vec::new(); 
 
-    for (i) in 0..compressed_chunks.len()
+    for i in 0..compressed_chunks.len()
     {
         let chunk_nbt: Chunk = get_chunk_nbt(&mut compressed_chunks[i].compressed_data).expect("Failure on chunk");
 
@@ -212,7 +244,34 @@ fn decode_chunks(compressed_chunks: &mut Vec<CompressedChunk>) -> Result<Vec<Chu
     Ok(decoded_chunks)
 }
 
-// todo: split
+fn parse_region_file_header(reader: &mut dyn Read) -> Result<RegionHeader, Error>
+{
+    // byte buffer for the location entries; initialize with 4096 0's. (0u8 i.e. a U8 with the value of zero repeated 4096 times)
+    let mut buffer = [0u8; DATA_SECTOR_SIZE];
+    let mut header_location_entries = Vec::new();
+
+    // Read 4096 bytes from reader into buffer
+    reader.read(&mut buffer)?;   
+
+    // Loop over buffer in chunks of 4 bytes (see wiki)
+    for byte in buffer.chunks(4)
+    {        
+        header_location_entries.push(RegionHeaderLocationEntry 
+        {
+            offset: big_endian!(&[0, byte[0], byte[1], byte[2]]),
+            sector_count: byte[3],
+        });
+    }
+
+    // Empty read away the timestamps. For future use
+    let mut tmp_buffer = [0u8; DATA_SECTOR_SIZE];
+    reader.read(&mut tmp_buffer);
+    
+    let region_header = RegionHeader {locations: header_location_entries};
+    
+    Ok(region_header)
+}
+
 fn parse_region_chunks(reader: &mut dyn Read, region_header: RegionHeader) -> Result<RegionChunks, Error>
 {
     let mut chunks_data = Vec::new();
@@ -224,13 +283,6 @@ fn parse_region_chunks(reader: &mut dyn Read, region_header: RegionHeader) -> Re
     Ok(RegionChunks{chunks: decoded_chunks})
 }
 
-
-fn get_blocks_in_chunk(region_chunks: &RegionChunks)
-{
-
-    println!("{:#?}", region_chunks.get_chunk(0, 0).sections[1]);
-}
-
 fn start() -> Result<u32, Error>
 {
     let mut reader = BufReader::new(File::open("r.0.0.mca") ?);
@@ -239,8 +291,8 @@ fn start() -> Result<u32, Error>
     let region_chunks: RegionChunks = parse_region_chunks(&mut reader, region_header).expect("Region chunks processing failed");
 
     // we get chunks by subchunk 16*16*16 to save memory allocation exceeding the cap
-    get_blocks_in_chunk(&region_chunks);
-    // println!("{:#?}", region_chunks.get_chunk(0, 0));
+    let chunk_blocks: Vec<Block> = get_blocks_in_chunk(&region_chunks, 0, 0);
+
     
     Ok(1)
 }
